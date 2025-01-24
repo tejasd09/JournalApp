848444313492399816b492bfa2f0ef2d64cc9cf1
tejasd09 <tejas1372003@gmail.com>
2025-01-25 00:03:17 +0530

    Delete Build/JournalApp directory

--- Build/JournalApp/repository/JournalEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.JournalApp.repository;
-
-import com.example.JournalApp.entity.JournalEntry;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
-}
